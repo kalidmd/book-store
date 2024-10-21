0de4948d7bb86437a078fb35509ab8f98d8e61bd
Kalid Mohammed <110621255+kalidmd@users.noreply.github.com>
2024-10-21 13:22:44 +0300

    /

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <main className='home-main'>
-        <h1>Home Pag e</h1>
+        <h1>Home Pag</h1>
     </main>
   )
 }
--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App';
-import PageNotFound from './components/pages/PageNotFound';
+import PageNotFound from './pages/PageNotFound'
 
 const router = createBrowserRouter([
     {
